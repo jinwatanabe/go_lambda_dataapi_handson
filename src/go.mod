1d1f49bc951496b4fbdf6c4d373ba603a7149ec7
jinwatanabe <jin.watanabe.6g@gmail.com>
2022-11-05 15:36:57 +0900

    dataAPI connect change

--- src/go.mod
+++ src/go.mod
@@ -6,14 +6,31 @@ require (
 	github.com/aws/aws-lambda-go v1.34.1
 	github.com/awslabs/aws-lambda-go-api-proxy v0.13.3
 	github.com/gin-gonic/gin v1.8.1
+	github.com/krotscheck/go-rds-driver v0.8.1
+	gorm.io/gorm v1.24.1
 )
 
 require (
+	github.com/aws/aws-sdk-go-v2 v1.16.3 // indirect
+	github.com/aws/aws-sdk-go-v2/config v1.15.5 // indirect
+	github.com/aws/aws-sdk-go-v2/credentials v1.12.0 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.12.4 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.10 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.4.4 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.3.11 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.9.4 // indirect
+	github.com/aws/aws-sdk-go-v2/service/rdsdata v1.12.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.11.4 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.16.4 // indirect
+	github.com/aws/smithy-go v1.11.2 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-playground/validator/v10 v10.10.0 // indirect
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/goccy/go-json v0.9.7 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
@@ -27,4 +44,5 @@ require (
 	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gorm.io/driver/mysql v1.4.3 // indirect
 )
--- src/handler/main.go
+++ src/handler/main.go
@@ -1,40 +1,71 @@
 package main
 
 import (
-    "log"
-    "context"
+	"context"
+	"fmt"
+	"time"
 
-    "github.com/aws/aws-lambda-go/events"
-    "github.com/aws/aws-lambda-go/lambda"
-    "github.com/awslabs/aws-lambda-go-api-proxy/gin"
-    "github.com/gin-gonic/gin"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	ginadapter "github.com/awslabs/aws-lambda-go-api-proxy/gin"
+	"github.com/gin-gonic/gin"
+	"github.com/krotscheck/go-rds-driver"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 var ginLambda *ginadapter.GinLambda
 
-func init() {
-    // stdout and stderr are sent to AWS CloudWatch Logs
-    log.Printf("Gin cold start")
-    r := gin.Default()
-    r.GET("/ping", func(c *gin.Context) {
-        c.JSON(200, gin.H{
-            "message": "CICD Check!",
-        })
-    })
-    r.GET("/hello", func(c *gin.Context) {
-        c.JSON(200, gin.H{
-            "message": "Hello World!",
-        })
-    })
+type User struct {
+	Name      string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
 
-    ginLambda = ginadapter.New(r)
+func init() {
+
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+
+		conf := &rds.Config{
+			ResourceArn: "RDSのリソースARN",
+			SecretArn:   "シークレットARN",
+			Database:    "handson",
+			AWSRegion:   "ap-northeast-1",
+			SplitMulti:  false,
+			ParseTime:   true,
+		}
+		dsn := conf.ToDSN()
+
+		DB, err := gorm.Open(mysql.New(mysql.Config{
+			DriverName: rds.DRIVERNAME,
+			DSN:        dsn,
+		}), &gorm.Config{})
+
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		err = DB.AutoMigrate(&User{})
+
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		c.JSON(200, gin.H{
+			"message": "Success!",
+		})
+
+	})
+
+	ginLambda = ginadapter.New(r)
 }
 
 func Handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-    // If no name is provided in the HTTP request body, throw an error
-    return ginLambda.ProxyWithContext(ctx, req)
+	// If no name is provided in the HTTP request body, throw an error
+	return ginLambda.ProxyWithContext(ctx, req)
 }
 
 func main() {
-    lambda.Start(Handler)
+	lambda.Start(Handler)
 }
